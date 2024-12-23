8f9859560f3b9b6f1a508ff7b5c498a766ccc0e7
drphnd <darrenpandiangan2322@gmail.com>
2024-12-23 20:49:15 +0700

    Setting View

---
+++ app/src/main/java/com/example/cicipinapp/views/SettingView.kt
@@ -0,0 +1,141 @@
+package com.example.cicipinapp.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cicipinapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingView() {
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(19.dp)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                    contentDescription = "Back Arrow",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(
+                    text = "Setting",
+                    color = Color.Black,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(innerPadding)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Profile Section
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.baseline_no_accounts_24),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "user123123",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Menu List
+            MenuItem(icon = R.drawable.baseline_account_circle_24, title = "Profile Setting")
+            MenuItem(icon = R.drawable.baseline_add_business_24, title = "Add Restaurant")
+            MenuItem(icon = R.drawable.baseline_sports_basketball_24, title = "Pusat Bantuan")
+            MenuItem(icon = R.drawable.baseline_logout_24, title = "Log Out")
+            MenuItem(icon = R.drawable.baseline_delete_24, title = "Delete Account")
+        }
+    }
+}
+
+@Composable
+fun MenuItem(icon: Int, title: String) {
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.weight(1f),
+                color = Color.Black
+            )
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = null,
+                tint = Color.Black
+            )
+        }
+        Divider(
+            color = Color.Gray,
+            thickness = 0.3.dp,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SettingViewPreview() {
+    SettingView()
+}
